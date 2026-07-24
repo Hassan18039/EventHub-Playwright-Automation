6a660e342b54add04aa6946a4c2b06449a29d125
Hassan <shehxad000@gmail.com>
2026-07-25 00:59:41 +0500

    feat: add negative test cases for user login validation

--- pages/auth/Login.page.ts
+++ pages/auth/Login.page.ts
@@ -6,6 +6,9 @@ export class LoginPage {
   readonly emailInput: Locator;
   readonly passwordInput: Locator;
   readonly signInBtn: Locator;
+  readonly errorMessage: Locator;
+  readonly emailValidationError: Locator;
+  readonly passwordValidationError: Locator;
 
   constructor(page: Page) {
     this.page = page;
@@ -13,6 +16,9 @@ export class LoginPage {
     this.emailInput = page.getByRole('textbox', { name: 'Email' });
     this.passwordInput = page.getByRole('textbox', { name: 'Password' });
     this.signInBtn = page.getByRole('button', { name: 'Sign In' });
+    this.errorMessage = page.getByText('Invalid email or password', { exact: true })
+    this.emailValidationError = page.getByText('Enter a valid email', { exact: true });
+    this.passwordValidationError = page.getByText('Password must be at least 6 characters', { exact: true });
   }
 
   async navigate() {
@@ -28,6 +34,19 @@ export class LoginPage {
     await this.emailInput.fill(email);
     await this.passwordInput.fill(password);
     await this.signInBtn.click();
+  }
+  
+  async passAndEmailErrorMessage() {
+
+    await expect(this.errorMessage).toBeVisible();
+  }
+
+  async emailValidationErrorMessage() {
+    await expect(this.emailValidationError).toBeVisible();
+  }
+
+  async passwordValidationErrorMessage() {
+    await expect(this.passwordValidationError).toBeVisible();
   }
 
 }
